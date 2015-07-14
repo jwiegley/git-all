d6d4a653c5dd93f10a70c08e81b184329c3f78e6
John Wiegley <johnw@newartisans.com>
2015-07-14 19:39:04 -0400

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,21 +1,18 @@
-{ cabal, cmdargs, hslogger, parallelIo, regexPosix, shelly
-, systemFileio, systemFilepath, text, transformers
+{ mkDerivation, base, cmdargs, hslogger, parallel-io, regex-posix
+, shelly, stdenv, system-fileio, system-filepath, text
+, transformers, unix
 }:
-
-cabal.mkDerivation (self: {
+mkDerivation {
   pname = "git-all";
   version = "1.5.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
   buildDepends = [
-    cmdargs hslogger parallelIo regexPosix shelly systemFileio
-    systemFilepath text transformers
+    base cmdargs hslogger parallel-io regex-posix shelly system-fileio
+    system-filepath text transformers unix
   ];
-  meta = {
-    homepage = "https://github.com/jwiegley/git-all";
-    description = "Determine which Git repositories need actions to be taken";
-    license = self.stdenv.lib.licenses.bsd3;
-    platforms = self.ghc.meta.platforms;
-  };
-})
+  homepage = "https://github.com/jwiegley/git-all";
+  description = "Determine which Git repositories need actions to be taken";
+  license = stdenv.lib.licenses.bsd3;
+}
